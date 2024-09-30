771e74583b51caf4988b4c79ef1b2407600ded4e
Olivier Tassinari <olivier.tassinari@gmail.com>
2024-09-30 16:20:45 +0200

    simplify

--- src/app/getThemeServerContext.ts
+++ src/app/getThemeServerContext.ts
@@ -2,14 +2,14 @@ import * as React from 'react';
 
 let themeServerContext = null as any;
 
-export default function getThemeServerContext() {
-  if (!themeServerContext) {
-    if (React.cache) {
-      themeServerContext = React.cache(() => ({ current: {} }));
-    } else {
-      themeServerContext = () => ({ current: {} });
-    }
-  }
+// If this API is available https://react.dev/reference/react/cache
+// The advantage is that it allows to isolate each page rendering.
+if (React.cache) {
+  themeServerContext = React.cache(() => ({ current: {} }));
+} else {
+  themeServerContext = () => ({ current: {} });
+}
 
+export default function getThemeServerContext() {
   return themeServerContext;
 }
